44086e55a0c31e37255b9a271ba585e43db19b15
Kaofang <104669022+Nawasawank@users.noreply.github.com>
2023-06-26 10:56:32 +0700

    dwdew

--- checkdata.py
+++ checkdata.py
@@ -1,62 +1,120 @@
 import mysql.connector
-import yaml
-
-with open('local.config', 'r') as file:
-    local_config = yaml.safe_load(file)
 
 db = mysql.connector.connect(
-    host=local_config["host"],
-    user=local_config["user"],
-    password=local_config["password"],
-    database=local_config["database"]
+    host="localhost",
+    user="root",
+    password="snsdforever9",
+    database="borrowingsystem"
 )
+print("Command typing:\nUser record : ur\nOverview of the year : ooty\nBorrowing record : br\nReturn record : rr\nCheck stuff owner : cso\nNot return stuff : nrs")
 
-
-print("name/times/name_times/stuff")
+#เช็คว่าคนนี้ยืมคืนอะไรไปบ้างกี่ครั้ง
+print("-------------------------------------------------")
 command = input("SELECT YOUR COMMAND : ")
-if command =="name":
+print("-------------------------------------------------")
+if command =="ur":
     Name_borrower = input("borrower name : ")
     mycursor = db.cursor()
-    sql = "SELECT stuff FROM borrow WHERE name = %s"
-    mycursor.execute(sql,(Name_borrower,))
-    myresult = mycursor.fetchall()
-    print(len(myresult),end="")
+    sql_stuff= "SELECT stuff FROM data WHERE name = %s"
+    mycursor.execute(sql_stuff,(Name_borrower,))
+    stuffresult = mycursor.fetchall()
+        
+    sql_date= "SELECT date FROM data WHERE name = %s"
+    mycursor.execute(sql_date,(Name_borrower,))
+    dateresult = mycursor.fetchall()
+
+    sql_status= "SELECT status FROM data WHERE name = %s"
+    mycursor.execute(sql_status,(Name_borrower,))
+    statusresult = mycursor.fetchall()
+
+    sql_check= "SELECT status FROM data WHERE status='borrow' AND name=%s"
+    mycursor.execute(sql_check,(Name_borrower,))
+    checkresult = mycursor.fetchall()
+    num = len(checkresult)
+    num_all = len(stuffresult)
+    num_return = num_all-num
+    print(num_all,end="")
     print(" times")
-    num = len(myresult)
+    print("borrow: "+str(num)+' times  '+"return: "+str(num_return)+' times')
+    print()
+    num = len(stuffresult)
     string={}
     for i in range(num):
-        string[i]=str(myresult[i][0])
-        print(string[i])
+        print(str(statusresult[i][0])+" "+str(dateresult[i][0])+"\n"+str(stuffresult[i][0]))       
+        print()
+    print("-------------------------------------------------")
 
-elif command =="times":
+
+#เช็คทั้งปีว่าของถูกยืมไปทั้งหมดกี่ครั้ง  
+elif command == "ooty":
+    year = input("Year: ")
     mycursor = db.cursor()
-    sql = "SELECT stuff FROM borrow"
+    sql = "SELECT date FROM data WHERE status LIKE 'borrow'"
     mycursor.execute(sql)
     myresult = mycursor.fetchall()
-    print(len(myresult),end="")
-    print(" times")
+    num = len(myresult)
+    
+    sql = "SELECT stuff FROM data WHERE date like %s AND status LIKE 'borrow'"
+    mycursor.execute(sql,('%' + year+ '%',))
+    result = mycursor.fetchall()
+    num = len(myresult)
+    dict = {}
+    #print(result)
+    for i in range(num):
+        if not result[i][0] in dict:
+            dict[result[i][0]]=1
+        else:
+            dict[result[i][0]]+=1
+    for key,value in dict.items():
+        print(key,value,"times")
+    print("-------------------------------------------------")
 
-
-elif command == "Check_Date":
+#วัน-เดือน-ปีมีใครยืมอะไรไปบ้าง
+elif command == "br":
+    Year = input("Year: ")
+    Month = input("Month: ")
+    Date = input("Date: ")
+    y_m_d = Year+"-"+Month+"-"+Date
+    sql = "SELECT stuff, name FROM data WHERE date LIKE %s AND status LIKE 'borrow'"
     mycursor = db.cursor()
-    sql = "SELECT YEAR(date) FROM borrow"
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
-    print(sql)
-    print(type(sql))
+    mycursor.execute(sql, ('%' + y_m_d + '%',))
+    result = mycursor.fetchall()
+    result_str = ""
+    for row in result:
+        result_str += f"{row[0]} - {row[1]}\n\n"
+    print("")
+    print(y_m_d)
+    print()
+    print(result_str)
+    print("-------------------------------------------------")
 
+#วัน-เดือน-ปีมีใครคืนอะไรไปบ้าง
+elif command == "rr":
+    Year = input("Year: ")
+    Month = input("Month: ")
+    Date = input("Date: ")
+    y_m_d = Year+"-"+Month+"-"+Date
+    sql = "SELECT stuff, name FROM data WHERE date LIKE %s AND status LIKE 'return'"
+    mycursor = db.cursor()
+    mycursor.execute(sql, ('%' + y_m_d + '%',))
+    result = mycursor.fetchall()
+    result_str = ""
+    for row in result:
+        result_str += f"{row[0]} - {row[1]}\n\n"
+    print("")
+    print(y_m_d)
+    print()
+    print(result_str)
+    print("-------------------------------------------------")
 
-    Date= input("Year/Month : ")
-    if Date =="Year":
-        Year = input("YEAR :")
-
-elif command=="check_owner":
+#เช็คstuff id ใครเป็นเจ้าของของอะไร
+elif command=="cso":
     Stuff_ID = input("stuff id : ")
     mycursor = db.cursor()
-    stuff_sql = "SELECT first_name FROM owner WHERE nstda_code = %s"
+    stuff_sql = "SELECT first_name FROM user WHERE nstda_code = %s"
     mycursor.execute(stuff_sql,(Stuff_ID,))
     stuff_myresult = mycursor.fetchall()
-    owner_sql = "SELECT last_name FROM owner WHERE nstda_code = %s"
+    owner_sql = "SELECT last_name FROM user WHERE nstda_code = %s"
     mycursor.execute(owner_sql,(Stuff_ID,))
     owner_myresult = mycursor.fetchall()
     num = len(stuff_myresult)
@@ -66,14 +124,55 @@ elif command=="check_owner":
         stuff_string[i]=str(stuff_myresult[i][0])
         owner_string[i]=str(owner_myresult[i][0])
         print(stuff_string[i],owner_string[i])
-  
+    print("-------------------------------------------------")
 
-    
+#เช็คว่าใครยังไม่คืนบ้าง
+elif command =="nrs":
 
-    
+    #เช้คตัวreturn ว่ามีกี่ตัว borrowว่ามีกี่ตัว ถ้าไม่เท่ากันคือยังไม่ได้คืน
 
-   
-mycursor.execute("SELECT id FROM borrow")
-myresult = mycursor.fetchall()
-##for row in myresult: 
-   ##print(row)
+    borrow_sql = "SELECT stuff FROM data WHERE status LIKE 'borrow'"
+    mycursor = db.cursor()
+    mycursor.execute(borrow_sql,)
+    result = mycursor.fetchall()
+    return_sql = "SELECT stuff FROM data WHERE status LIKE 'return'"
+    mycursor = db.cursor()
+    mycursor.execute(return_sql,)
+    myresult = mycursor.fetchall()
+    dict_borrow={}
+    dict_return={}
+    num_borrow = len(result)
+    num_return = len(myresult)
+    for i in range(num_borrow):
+        #print(result[i][0])
+        #print(type(result[i][0]))
+        dict_borrow[result[i][0]]=0
+        dict_return[result[i][0]]=0
+
+    for i in range(num_borrow):
+        if not result[i][0] in dict_borrow:
+            dict_borrow[result[i][0]]=0
+        else:
+            dict_borrow[result[i][0]]+=1
+    for i in range(num_return):
+        if not myresult[i][0] in dict_return:
+            dict_return[myresult[i][0]]=0
+        else:
+            dict_return[myresult[i][0]]+=1
+
+    list =[]
+    for i in range(num_borrow):
+        if not dict_borrow[result[i][0]]==dict_return[result[i][0]]:
+            if not result[i][0] in list:
+                list.append(result[i][0])
+    num = len(list)
+    for i in range(num):
+        namesql = "SELECT name FROM data WHERE stuff LIKE %s AND status LIKE 'borrow' ORDER BY sequence DESC LIMIT 1" 
+        mycursor = db.cursor()
+        mycursor.execute(namesql, (list[i],))
+        myresult = mycursor.fetchall()
+        print(int(i + 1), end=". ")
+        print(list[i] + "\nborrow by: ", end="")
+        for row in myresult:
+            print(row[0])
+        print("-------------------------------------------------")
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import mysql.connector
-
+import datetime
+from flask import request
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -8,36 +9,120 @@ mydb = mysql.connector.connect(
     database="borrowingsystem"
 )
 
-mycursor = mydb.cursor()
 
 
+def insert_data(name, id,tel, string_data, Owner):
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="snsdforever9",
+    database="borrowingsystem")
 
+    now = datetime.datetime.now()
+    mycursor = mydb.cursor()
+    # Execute the query to get the existing records
+    sql = "SELECT id FROM data"
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    num = len(myresult)
+    sequence = num + 1
 
+    #connect stuff
+    owner_sql = "SELECT nstda_code FROM user"
+    mycursor.execute(owner_sql)
+    owner_myresult = mycursor.fetchall()
+    count = 0
+    num = len(owner_myresult)
+    for i in range(num):
+        if string_data==owner_myresult[i][0]:
+            count = int(i)
+    stuff_sql = "SELECT first_name FROM user"
+    mycursor.execute(stuff_sql)
+    stuff_myresult = mycursor.fetchall()
+    stuff = stuff_myresult[count][0]
+    
 
+    # Insert new data into the table
+    sql = "INSERT INTO data (sequence, id, name, stuff, tel, date, qr, owner, status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    values = (sequence, id, name, stuff, tel, now.strftime('%Y-%m-%d %H:%M:%S'), string_data, Owner, "borrow")
+    mycursor.execute(sql, values)
+    mydb.commit()
 
-mycursor.execute("CREATE TABLE data (sequence VARCHAR(255), id VARCHAR(255), name VARCHAR(255),tel VARCHAR(255), stuff VARCHAR(255),date timestamp,owner VARCHAR(255),qr VARCHAR(255),status VARCHAR(255))")
+    mycursor.close()
+    mydb.close()
 
-#mycursor.execute("CREATE DATABASE borrowing_system")
+def output_data(string_data):
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="snsdforever9",
+    database="borrowingsystem")
+    
+    now = datetime.datetime.now()
+    mycursor = mydb.cursor()
+    # Execute the query to get the existing records
+    sql = "SELECT id FROM data"
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    num = len(myresult)
+    sequence = num + 1
 
-#mycursor.execute("SELECT * FROM owner WHERE nstda_code = '9000-001-0001-000001887' ")
+    sql = "SELECT name FROM data WHERE qr = %s"
+    mycursor.execute(sql,(string_data,))
+    myresult = mycursor.fetchall()
+    name = myresult[-1][0]
+    print(name)
 
-#myresult = mycursor.fetchall()
+    sql = "SELECT id FROM data WHERE qr = %s"
+    mycursor.execute(sql,(string_data,))
+    myresult = mycursor.fetchall()
+    id = myresult[-1][0]
 
+    sql = "SELECT tel FROM data WHERE qr = %s"
+    mycursor.execute(sql,(string_data,))
+    myresult = mycursor.fetchall()
+    tel = myresult[-1][0]
 
-#for result in myresult:
-   # print(result)
+    sql = "SELECT stuff FROM data WHERE qr = %s"
+    mycursor.execute(sql,(string_data,))
+    myresult = mycursor.fetchall()
+    stuff = myresult[-1][0]
 
-#query="ALTER TABLE user ADD avaliable VARCHAR(100)"
-#mycursor.execute(query)
-#mydb.commit()
+    sql = "SELECT owner FROM data WHERE qr = %s"
+    mycursor.execute(sql,(string_data,))
+    myresult = mycursor.fetchall()
+    Owner = myresult[-1][0]
 
-#sql = "DELETE FROM data WHERE id = '1103703686615'"
+    # Insert new data into the table
+    sql = "INSERT INTO data (sequence, id, name, stuff, tel, date, qr, owner, status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    values = (sequence, id, name, stuff, tel, now.strftime('%Y-%m-%d %H:%M:%S'), string_data, Owner, "return")
+    mycursor.execute(sql, values)
+    mydb.commit()
 
-#mycursor.execute(sql)
+    mycursor.close()
+    mydb.close()
 
-#db.commit()
+def alldata():
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="snsdforever9",
+    database="borrowingsystem")
 
-#sql = "UPDATE user SET avaliable = 'True'"
-#mycursor.execute(sql)
-
-#mydb.commit()
+    mycursor = mydb.cursor()
+    sql = "SELECT * FROM data WHERE status = 'borrow' "
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    if len(myresult) > 0: 
+        for x in myresult:
+            arr = {
+                "sequence" : x[0],
+                "id" : x[1],
+                "name" : (x[2]),
+                "tel" : x[3],
+                "stuff" : x[4],
+                "date" : x[5],
+                "owner" : x[6],
+                "qr" : x[7]
+                }
+    return arr
